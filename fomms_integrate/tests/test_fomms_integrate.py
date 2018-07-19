59637796e4f7aaa7f3d859450487e5a9c525f7dd
Michael Quevillon <mquevill@nd.edu>
2018-07-19 16:49:13 -0400

    Update tests

--- fomms_integrate/tests/test_fomms_integrate.py
+++ fomms_integrate/tests/test_fomms_integrate.py
@@ -6,6 +6,7 @@ Unit and regression test for the fomms_integrate package.
 import fomms_integrate
 import pytest
 import sys
+import numpy as np
 
 def test_fomms_integrate_imported():
     """Sample test, will always pass so long as import statement worked"""
@@ -26,20 +27,20 @@ def volume(x):
 
 def test_trapz():
     x = np.array([0, 10])
-    I = integrate.trapz(x, g)
+    I = fomms_integrate.newton_cotes.trapz(x, g)
     assert I == 150.00
 
 def test_monte1d():
     x = np.array([0, 3])
-    I = integrate.monte_1d(x, f, 100000)
+    I = fomms_integrate.newton_cotes.monte_1d(x, f, 100000)
     assert np.allclose(I, 9.00, 1e-2)
 
 def test_simpson():
     x = np.array([0, 3])
-    I = integrate.simpson(x, f)
+    I = fomms_integrate.newton_cotes.simpson(x, f)
     assert I == 9.00
     
 def test_monte2d():
     domain = np.array([[-1, -1], [1, 1]])
-    I = integrate.monte_2d(h, volume, domain, 1000000)
+    I = fomms_integrate.newton_cotes.monte_2d(h, volume, domain, 1000000)
     assert np.allclose(I, np.pi, 1e-2)
