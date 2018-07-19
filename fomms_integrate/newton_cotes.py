2758d0697f0bd1d253ef36c2c040bc3f50172118
Michael Quevillon <mquevill@nd.edu>
2018-07-19 18:21:32 -0400

    Add Simpson's 3/8 and Boole's rules

--- fomms_integrate/newton_cotes.py
+++ fomms_integrate/newton_cotes.py
@@ -46,3 +46,51 @@ def simpson(x, f):
     yc = f(b)
     I = (b-a) * (ya + 4 * yb + yc) / 6
     return I
+
+def simpson3_8(x, f):
+    """ 
+    Compute a 1D definite integral using the 3/8 Simpson's rule.
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
+    Returns
+    -------
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f((2*a+  b)/3)
+    yc = f((  a+2*b)/3)
+    yd = f(b)
+    I = (b-a) * (ya + 3 * (yb + yc) + yd) / 8
+    return I
+
+def boole(x, f):
+    """ 
+    Compute a 1D definite integral using the 3/8 Simpson's rule.
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
+    Returns
+    -------
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f((3*a+  b)/4)
+    yc = f((  a+  b)/2)
+    yd = f((  a+3*b)/4)
+    ye = f(b)
+    I = (b-a) * (7 * (ya + ye) + 32 * (yb + yd) + 12 * yc) * 2 / 45
+    return I
+
